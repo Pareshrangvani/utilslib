0699f533d53e5d4a505051ff2bc897779d43751e
Pareshrangvani <65137136+Pareshrangvani@users.noreply.github.com>
2022-03-01 19:52:44 +0530

    add ops params and required date functions

--- utilslib.py
+++ utilslib.py
@@ -5,7 +5,7 @@ import json
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from mysql_mgr import *
-
+from datetime import datetime, date, timedelta
 
 def invoke_http_request(endpoint, method, headers, payload=None, json_data=None, timeout=61):
     """ here two exception block. one is for request exception and other is for json decoder exception.
@@ -64,3 +64,40 @@ def log_failed_http_request(endpoint, response, status_code):
 
 def is_success_request(status_code):
     return 200 <= status_code <= 299
+
+ops = {
+    **BUILTINS,
+    'starts_with': lambda data, a, b: a.startswith(b),
+    'ends_with': lambda data, a, b: a.endswith(b),
+    'date_between': lambda data, a, b, c: str_to_datetime(b) <= str_to_datetime(a) <= str_to_datetime(c),
+    'date_within_next': lambda data, a, b, c: date_within_next(a, b, c),
+    'date_within_last': lambda data, a, b, c: date_within_last(a, b, c),
+    'date_after': lambda data, a, b: str_to_datetime(a) > str_to_datetime(b),
+    'date_before': lambda data, a, b: str_to_datetime(a) < str_to_datetime(b),
+    'date_yesterday': lambda data, a: str_to_datetime(a).date() == date.today() - timedelta(days=1),
+    'date_today': lambda data, a: str_to_datetime(a).date() == date.today(),
+    'date_tomorrow': lambda data, a: str_to_datetime(a).date() == date.today() + timedelta(days=1),
+    'date_is_empty': lambda data, a: a == ""
+}
+
+
+def date_within_next(date, number, period):
+    if period == "days":
+        return datetime.now() <= str_to_datetime(date) <= (
+                datetime.now() + timedelta(days=int(number)))
+    elif period == "weeks":
+        return datetime.now() <= str_to_datetime(date) <= (
+                datetime.now() + timedelta(weeks=int(number)))
+
+
+def date_within_last(date, number, period):
+    if period == "days":
+        return (datetime.now() - timedelta(
+            days=int(number))) <= str_to_datetime(date) <= datetime.now()
+    elif period == "weeks":
+        return (datetime.now() - timedelta(
+            weeks=int(number))) <= str_to_datetime(date) <= datetime.now()
+
+
+def str_to_datetime(date_time, str_format="%Y-%m-%d %H:%M:%S"):
+    return datetime.strptime(date_time, str_format)
